7d0691493c94b9e1e684441c40e72d707e46aa53
Julian Toelle <julian.toelle97@gmail.com>
2015-04-24 20:48:21 +0200

    Moved src to core-src

--- core-src/com/toelle/muzikant/manager/MusicLibrary.java
+++ core-src/com/toelle/muzikant/manager/MusicLibrary.java
@@ -5,6 +5,7 @@ import com.toelle.muzikant.database.IOHandler;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -22,6 +23,13 @@ public class MusicLibrary {
     public List<Song> search(Predicate<? super Song> predicate) {
         return musiclibrary.parallelStream()
                             .filter(predicate)
+                            .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public List<TagElement> getDistinctTags(Function<? super Song, TagElement> mapper) {
+        return musiclibrary.parallelStream()
+                            .map(mapper)
+                            .distinct()
                             .collect(Collectors.toCollection(ArrayList::new));
     }
 
---
+++ core-src/com/toelle/muzikant/manager/TagElement.java
@@ -0,0 +1,4 @@
+package com.toelle.muzikant.manager;
+
+public class TagElement {
+}
