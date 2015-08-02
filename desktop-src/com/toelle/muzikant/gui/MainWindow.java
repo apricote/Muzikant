ecbb66596f9c15bd48f12e11b7108344723764ac
Julian Tölle <julian.toelle97@gmail.com>
2015-08-02 15:37:18 +0200

    Property loading

--- desktop-src/com/toelle/muzikant/gui/MainWindow.java
+++ desktop-src/com/toelle/muzikant/gui/MainWindow.java
@@ -10,12 +10,21 @@ import com.toelle.muzikant.playback.Plattenspieler;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Properties;
 
 public class MainWindow extends Application {
 
     private Deejay deejay;
     private MusicLibrary library;
+
+    private final String[] CFG_LOCATION = {System.getProperty("user.home"), ".muzikant"};
+    private final String[][] STANDARD_CFG = {{"music.location", Paths.get(System.getProperty("user.home"), "Music").toString()}};
 
     public static void main(String[] args) {
         launch(args);
@@ -23,6 +32,7 @@ public class MainWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        getProperties();
 
     }
 
@@ -48,5 +58,32 @@ public class MainWindow extends Application {
 
     private Plattenspieler getPlattenspieler() {
         return new DesktopPlattenspieler();
+    }
+
+    private void getProperties() {
+        Path configPath = Paths.get(CFG_LOCATION[0], Arrays.copyOfRange(CFG_LOCATION, 1, CFG_LOCATION.length));
+        // Ugly because of the one required parameter and varargs after that
+
+        Properties props = new Properties();
+
+        try {
+            props.load(new FileInputStream(configPath.toFile()));
+        } catch (IOException e) {
+            //TODO Log
+
+            //Setting standard properties because no config file exists
+            Arrays.stream(STANDARD_CFG).forEach(cfg -> props.setProperty(cfg[0], cfg[1]));
+
+            //Saving to file
+            try {
+                configPath.toFile().createNewFile();
+                props.store(new FileOutputStream(configPath.toFile()), "Muzikant Player Preferences");
+            } catch (IOException e1) {
+                //TODO Log
+                e1.printStackTrace();
+            }
+            //TODO Log
+            e.printStackTrace();
+        }
     }
 }
