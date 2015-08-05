6b466ccaf731630c931968d51318b681be347ad5
Julian Tölle <julian.toelle97@gmail.com>
2015-08-05 15:20:14 +0200

    Added documentation

--- core-src/com/toelle/muzikant/database/IOHandler.java
+++ core-src/com/toelle/muzikant/database/IOHandler.java
@@ -5,11 +5,36 @@ import com.toelle.muzikant.model.Song;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Provides accesss to all Song files that are in the current Song Database. It
+ * is up to the implementation of this interface how exactly this database
+ * should look like and how it is implemented.
+ */
 public interface IOHandler {
 
+    /**
+     * Inserts the given Song into the database and writes it to disk.
+     *
+     * @param song the song to be saved
+     * @throws IOException
+     */
     void put(Song song) throws IOException;
 
+    /**
+     * Inserts the given list of Songs into the database and writes all of them
+     * to the disk.
+     *
+     * @param songs a list of songs to be saved
+     * @throws IOException
+     */
     void put(List<Song> songs) throws IOException;
 
+    /**
+     * Returns a List of all Songs that are in the current database.
+     * The meta information of every song is included as much as possible.
+     *
+     * @return all Songs in database
+     * @throws IOException
+     */
     List<Song> getAll() throws IOException;
 }
--- core-src/com/toelle/muzikant/manager/MusicLibrary.java
+++ core-src/com/toelle/muzikant/manager/MusicLibrary.java
@@ -10,23 +10,66 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+/**
+ * This class manages a List of Songs and offers methods to search through it.
+ */
 public class MusicLibrary {
 
     private List<Song> musiclibrary;
 
     private IOHandler ioHandler;
 
+    /**
+     * Instantiate a new MusicLibrary with the given IOHandler. The IOHandler
+     * will be used to retrieve all Songs from Database.
+     *
+     * @param ioHandler the IOHandler that should be used
+     * @throws IOException
+     */
     public MusicLibrary(IOHandler ioHandler) throws IOException {
         this.ioHandler = ioHandler;
         this.musiclibrary = ioHandler.getAll();
     }
 
+    /**
+     * Lets you search through the list of Songs based on the given
+     * <code>predicate</code>.
+     *
+     * Example:
+     * <pre>
+     * <code>MusicLibrary ml = new MusicLibrary(new IOHandler());
+     *
+     * ml.search(s -> s.getAlbumName() == "Mutter"); // Returns all Songs that are in an Album called "Mutter"
+     *
+     * ml.search(s -> s.getTitle().contains("Submarine")); // Returns all Songs whose title contains the String "Submarine"</code></pre>
+     * @param predicate the predicate that should be used for search
+     * @return a list of Songs that match the predicate
+     */
     public List<Song> search(Predicate<? super Song> predicate) {
         return musiclibrary.parallelStream()
                             .filter(predicate)
                             .collect(Collectors.toCollection(ArrayList::new));
     }
 
+    /**
+     * Maps all the Songs into distinct tags based on the given Mapping function.
+     *
+     * Example:
+     * <pre>
+     * <code>MusicLibrary ml = new MusicLibrary(new IOHandler());
+     *
+     * List{@code<String>} artists = ml.getDistinctTags(s -> s.getArtistName()); // Returns a List of all Artists in the Music Library
+     * </code></pre>
+     *
+     * <b>Warning</b>: Should not be used to get distinct lists of Songnames or
+     * Albumnames as two different Songs/Albums by different Artists can be
+     * named the same and still be two distinct Entities. <b>In that case this
+     * method will only return one entry.</b>
+     *
+     * @param mapper the function to modify the Songstream (e.g. turn it into Artist Strings)
+     * @param <T> The type of the resulting List (e.g. String in case of Artist)
+     * @return A list of distinct tags
+     */
     public <T> List<T> getDistinctTags(Function<? super Song, T> mapper) {
         return musiclibrary.parallelStream()
                             .map(mapper)
